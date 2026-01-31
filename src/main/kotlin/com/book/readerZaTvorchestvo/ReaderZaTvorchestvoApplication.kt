c95eb466cc369695a8bc36e93ba653af21d8c6f5
Komarov Andrey <kkmrvvv@gmail.com>
2026-01-31 06:35:39 +0300

    init

---
+++ src/main/kotlin/com/book/readerZaTvorchestvo/ReaderZaTvorchestvoApplication.kt
@@ -0,0 +1,11 @@
+package com.book.readerZaTvorchestvo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReaderZaTvorchestvoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReaderZaTvorchestvoApplication>(*args)
+}
---
+++ src/test/kotlin/com/book/readerZaTvorchestvo/ReaderZaTvorchestvoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.book.readerZaTvorchestvo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReaderZaTvorchestvoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
